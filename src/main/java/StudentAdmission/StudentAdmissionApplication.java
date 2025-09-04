bc06ea2cc7df93c72fe90930ba83312242ccad78
narayan.rguttal@thermofisher.com <narayan.rguttal@thermofisher.com>
2025-09-04 13:04:14 +0530

    test

---
+++ src/main/java/StudentAdmission/StudentAdmissionApplication.java
@@ -0,0 +1,13 @@
+package StudentAdmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAdmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAdmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/StudentAdmission/StudentAdmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package StudentAdmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAdmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
